307de4018c40a066e91fecf5f3348061ce9bb16c
Aditya Yadav <adityaydv12890@gmail.com>
2025-06-13 12:48:05 +0530

    Create 460A - Vasya and Socks.cpp

---
+++ 900 Rated Problems/460A - Vasya and Socks.cpp
@@ -0,0 +1,27 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n,m;
+    cin>>n>>m;
+    
+    int socks = n;
+    int days = 0;
+
+    while(socks > 0) {
+        days++;
+        socks--;
+
+        if(days % m == 0) {
+            socks++;
+        }
+    }
+
+    cout<<days<<endl;
+}
